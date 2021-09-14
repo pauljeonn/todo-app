b342c88dafa87189cc9cc7f26ae880bec3b3e463
Paul Jeon <pauljeonn@gmail.com>
2021-09-14 20:15:14 +0900

    add todoTemplate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import TodoTemplate from './components/todoTemplate/TodoTemplate';
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			{/* <TodoTemplate>안녕하세요!</TodoTemplate> */}
+			<TodoTemplate children="안녕하세요" />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/todoTemplate/TodoTemplate.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './todoTemplate.scss';
+
+const TodoTemplate = ({ children }) => {
+	return <div className="todo-template">{children}</div>;
+};
+
+export default TodoTemplate;
