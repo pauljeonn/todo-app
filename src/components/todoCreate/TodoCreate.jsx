44fd0e311d441a3e9f8eb14192b47092d73aa6b4
Paul Jeon <pauljeonn@gmail.com>
2021-09-16 23:58:10 +0900

    add todoCreate component

---
+++ src/components/todoCreate/TodoCreate.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './todoCreate.scss';
+
+const TodoCreate = () => {
+	return <div className="todo-create"></div>;
+};
+
+export default TodoCreate;
--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -6,6 +6,8 @@ const TodoList = () => {
 	return (
 		<div className="todo-list">
 			<TodoItem text="설거지하기" done={true} />
+			<TodoItem text="자바스크립트 공부하기" done={true} />
+			<TodoItem text="프로젝트 생성하기" done={true} />
 		</div>
 	);
 };
